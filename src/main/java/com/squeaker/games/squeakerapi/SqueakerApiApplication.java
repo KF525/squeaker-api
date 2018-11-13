06d355a04b18ee09db99adcc4836b13cdf8f9147
Kate Fulton <kfulton@marchex.com>
2018-11-12 17:34:19 -0800

    Post Spring Initlizr commit

---
+++ src/main/java/com/squeaker/games/squeakerapi/SqueakerApiApplication.java
@@ -0,0 +1,12 @@
+package com.squeaker.games.squeakerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqueakerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqueakerApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/squeaker/games/squeakerapi/SqueakerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.squeaker.games.squeakerapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SqueakerApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
